a6093ac46bce2dd20fcfa516ce9cb47fda2abe38
Taste <ajtaste-001@outlook.jp>
2026-01-10 08:19:09 +0000

    keydown→keyup

--- public/sites.google.com/script.js
+++ public/sites.google.com/script.js
@@ -12,7 +12,7 @@ function initGame() {
   const status = document.getElementById("status");
 
   // ページ全体でキー入力を監視
-  document.addEventListener("keydown", function (event) {
+  document.addEventListener("keyup", function (event) {
     const key = event.key;
 
     // 数字キー以外は無視
@@ -23,9 +23,9 @@ function initGame() {
     // 正解の1文字と比較
     if (key === PI_STR[index]) {
       index++;
-      status.textContent = `正解！ 現在 ${index} 桁目まで正解`;
+      status.textContent = `現在${index}`;
     } else {
-      status.textContent = `不正解。${index} 桁目まで正解でした。最初からやり直し。`;
+      status.textContent = `結果${index}`;
       index = 0;
     }
   });
